63f1cc8fd503a53fdc131034d9b7052ae81a770a
Caio Herrera <caio.herrera@outlook.com>
2022-02-26 19:20:09 -0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -55,6 +57,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader()
+                                        .AllowAnyMethod()
+                                        .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
