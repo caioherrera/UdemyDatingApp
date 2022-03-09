dcf6f9627de86072207bec37137e220cd2e19461
Caio Herrera <caio.herrera@outlook.com>
2022-03-09 13:03:34 -0300

    End of section 5

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,6 +7,7 @@ namespace API.Entities
 {
     public class AppUser
     {
+        // Default user: bob, Pa$$w0rd
         public int Id { get; set; }
         public string UserName { get; set; }   
         public byte[] PasswordHash { get; set; }
